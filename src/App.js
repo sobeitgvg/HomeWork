4edbd16b057e49deaac81ac310d6e0f84e066aa0
sobeitgvg <gvg.sobeit@yandex.ru>
2022-04-27 16:45:50 +0300

    test

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
 
+import './App.css';
+import { Fragment } from 'react';
+import {Button} from './components/massage/massage'
+ 
+
+
+function App() {
+    let greet = 'H-A-L-L-O'
+    const handleBtn = (text) =>{
+    alert(`${text}`)
+    }
+    
+      return (
+        <Fragment>
+          <Button onButtonClick = {handleBtn} title={greet}/>
+        </Fragment>
+      ); 
+}
 
 export default App;
