4bb2fd79a196ab90961d1ca8f8056f70faf3f3d9
sobeitgvg <gvg.sobeit@yandex.ru>
2022-05-19 14:52:16 +0300

    les3

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
 
 import './App.css';
 import {Fragment } from 'react';
-import {Button} from './components/massage/massage'
-import {Massagelist} from './components/massage/massagelist'
+import {Massagelist} from './components/massage/massagelist';
 
 
 
 function App() {
-    let greet = 'H-A-L-L-O'
-    const handleBtn = (text) =>{
-    alert(`${text}`)
-    }
-    
+
       return (
         <Fragment>
-          <Button onButtonClick = {handleBtn} title={greet}/>
           <Massagelist/>
         </Fragment>
       ); 
--- src/components/massage/form.jsx
+++ src/components/massage/form.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 import './massage.css';
+import { TextField, Button } from '@mui/material';
+
 
 export const Form = ({onSubmit}) => {
+
+    const inputRef = React.useRef(null);
     const [massage, setMassage] = React.useState('')
     const handleMassage = (e) => {
         setMassage (e.target.value)
@@ -10,12 +14,17 @@ export const Form = ({onSubmit}) => {
         e.preventDefault()
         setMassage ('')
         onSubmit(massage)
+        
     }
+    React.useEffect(()=>{
+        inputRef.current.focus();
+    }, []);
+
     return(
         <div>
-        <form onSubmit = {handleSubmit}>
-            <input type = 'text' value = {massage} onChange = {handleMassage}/>
-            <input type = 'submit' className='btn-sbt'/>
+        <form  onSubmit = {handleSubmit}>
+            <TextField ref={inputRef} label = 'Сообщение' variant="outlined" type = 'text' value = {massage} onChange = {handleMassage} autoFocus = 'true'/>
+            <Button sx={{height:55}} variant="contained" type = 'submit'>Отправить</Button>
         </form>
         </div>
     )
